86ebf8270758ff3c70b52e638781ad9f02771c09
ronit-lab <62502181+ronit-lab@users.noreply.github.com>
2020-04-03 21:34:31 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,1 @@
-in order to run game pygame should be install. Then simply run game in jupyter notebook
+in order to run game pygame should be install. Then simply run game in jupyter notebook and whenever you want to quit in betwwen simply cross the tab and press space bar system will exit 
